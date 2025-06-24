85e325294703408dc01f55e93a7a53622e887754
Aki <141536869+AlexAutumns@users.noreply.github.com>
2025-06-24 18:26:02 +0400

    frontend dashboard v0.1

---
+++ Frontend/app/routes/dashboard/pages/overviewPages/adminOverview.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const adminOverview = () => {
+    return (
+        <div>
+            {/* Top Part of the Overview Page */}
+            <div>admin</div>
+            {/* Main Chart */}
+            <div></div>
+            {/* Delivery Management Table */}
+            <div></div>
+        </div>
+    );
+};
+
+export default adminOverview;
